ef69d31ee054253f8a25480f92504f1721c65f92
Ioana Bondoc <ioana.bondoc17@gmail.com>
2019-05-22 15:45:27 +0300

    Proiect LFA:Automat finit determinist(Visual Studio+ppt)

---
+++ DFA/DFA.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DFA
+{
+    class DFA
+    {
+        private char[] States;
+        private char[] FinalStates;
+        private char StartState { get; }
+        private char[,] Table;
+        //alfabet={0,1}
+        public DFA()
+        {
+            States = new char[] { 'A', 'B', 'C', 'D','E' };
+            FinalStates = new char[] { 'E' };
+            StartState = 'A';
+            InitTable(@"../../table.txt");
+            
+        }
+        private void InitTable(string path)
+        {
+            Table = new char[2, 5];
+            TextReader tr = new StreamReader(path);
+            string buffer;
+            string[] elems;
+            int row = 0;
+            while ((buffer=tr.ReadLine())!=null)
+            {
+                //Console.WriteLine(buffer);
+                elems=buffer.Split(' ');
+                for (int col = 0; col < elems.Length; col++)
+                {
+                    //Console.Write(elems[col]);
+                    Table[row, col] = char.Parse(elems[col]);
+                    //Console.Write(Table[row, col] + " ");
+                }
+                //Console.WriteLine();
+                row++;
+            }
+        }
+        public bool CheckWord(string word)
+        {
+            char CurrentState = StartState;
+            for (int i = 0; i < word.Length; i++)
+            {
+                switch(word[i])
+                {
+                    case '0':
+                        CurrentState = Table[0, CurrentState - 65];
+                        break;
+                    case '1':
+                        CurrentState = Table[1, CurrentState - 65];
+                        break;
+                    default:
+                        Console.WriteLine("Wrong letters! (Only 0 or 1)");
+                        return false;
+                }
+                
+            }
+            foreach (int State in FinalStates)
+                if (CurrentState == State)
+                    return true;
+            return false;
+        }
+    }
+}
---
+++ DFA/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This automat checks all the words that contain '1011'");
+            while (true)
+            {
+                Console.Write("Give the word: ");
+                string word = Console.ReadLine();
+                //Console.WriteLine(word);
+                DFA dfa = new DFA();
+                Console.WriteLine(dfa.CheckWord(word)); 
+            }
+        }
+    }
+}
